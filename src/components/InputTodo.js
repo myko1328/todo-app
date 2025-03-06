303f963ea9ae44eb64e90b9f7b04fc26f6bbe62a
Myko Miparanum <m.miparanum@gmail.com>
2025-03-06 16:08:48 +0800

    added validation to input, user cannot add if the input is empty

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,30 +2,42 @@ import React, { useState } from 'react';
 
 const InputTodo = ({ addTodoProps }) => {
   const [title, setTitle] = useState('');
+  const [isRequired, setIsRequired] = useState(false);
 
   const onChange = (e) => {
-    console.log(e);
+    e.persist();
     setTitle(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodoProps(title);
-    setTitle('');
+
+    if (title.trim()) {
+      addTodoProps(title);
+      setTitle('');
+      setIsRequired(false);
+    } else {
+      setIsRequired(true);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <input
-        type="text"
-        className="input-text"
-        placeholder="Add todo..."
-        value={title}
-        name="title"
-        onChange={onChange}
-      />
-      <input type="submit" className="input-submit" value="Submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={title}
+          name="title"
+          onChange={onChange}
+        />
+        <input type="submit" className="input-submit" value="Submit" />
+      </form>
+      {isRequired && (
+        <span style={{ fontSize: '12px', color: 'red' }}>This is required</span>
+      )}
+    </>
   );
 };
 
