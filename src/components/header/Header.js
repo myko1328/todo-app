ae8736785f13b8af24ce642ca28b38fdf21baade
Myko Miparanum <m.miparanum@gmail.com>
2025-03-06 16:53:28 +0800

    separate style for header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,12 @@
-import React from "react";
+import React from 'react';
+
+import './index.css';
 
 const Header = () => {
-  const headerStyle = {
-    padding: "20px 0",
-    lineHeight: "2em"
-  };
   return (
-    <header style={headerStyle}>
-      <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
-        Simple Todo App
-      </h1>
-      <p style={{ fontSize: "19px" }}>
-        Please add to-dos item(s) through the input field
-      </p>
+    <header className="header-container">
+      <h1>Simple Todo App</h1>
+      <p>Please add to-dos item(s) through the input field</p>
     </header>
   );
 };
